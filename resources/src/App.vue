230a1fe990916511b4f433a27e6a5472c0a41649
13kaizo <informaticakaizo@gmail.com>
2023-06-06 20:01:31 +0200

    Añadir Vue al proyecto de Laravel

---
+++ resources/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Laravel 9 + Vue 3</h1>
+<hr>
+  <h2> {{ title }}</h2>
+</template>
+
+<script setup>
+const title="Esto es una puta tienda con Laravel y Vue";
+</script>
+
+<style scoped>
+
+h2{
+    text-align: center;
+}
+
+</style>
+
